7a527cd8fac23d8d9a6b4c28732cc1f000c04ef7
Luthfi Alfarisi <luthfialfarizi98@gmail.com>
2019-01-12 22:24:15 +0700

    Update 12 Jan

--- app/src/main/java/com/wysiwyg/temanolga/activities/MainActivity.kt
+++ app/src/main/java/com/wysiwyg/temanolga/activities/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.wysiwyg.temanolga.activities
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import android.view.View
 import com.google.firebase.auth.FirebaseAuth
 import com.wysiwyg.temanolga.R
@@ -13,15 +15,23 @@ import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private val home = HomeFragment()
+    private val explore = ExploreFragment()
+    private val message = MessageFragment()
+    private val notif = NotificationFragmentManager()
+    private val profile = ProfileFragment()
+    private var active: Fragment = home
+    private val fm = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         isLogin()
-        initNavigation(savedInstanceState)
+        initNavigation()
+        initFragment()
         fab.addEvent()
-
     }
 
     private fun View.addEvent() {
@@ -35,43 +45,53 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun loadFragment(savedInstanceState: Bundle?, fragment: Fragment, title: Int) {
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.content, fragment, fragment::class.java.simpleName)
-                .commit()
-        }
+    private fun initFragment() {
+        fm.beginTransaction().add(R.id.content, profile, "5").hide(profile).commit()
+        fm.beginTransaction().add(R.id.content, notif, "4").hide(notif).commit()
+        fm.beginTransaction().add(R.id.content, message, "3").hide(message).commit()
+        fm.beginTransaction().add(R.id.content, explore, "2").hide(explore).commit()
+        fm.beginTransaction().add(R.id.content, home, "1").commit()
+    }
+
+    private fun loadFragment(fragment: Fragment, title: Int) {
+        fm.beginTransaction().hide(active).show(fragment).commit()
+        active = fragment
         tv_toolbar_title.text = resources.getString(title)
     }
 
-    private fun initNavigation(savedInstanceState: Bundle?) {
-        navigation.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                navigation_home -> {
-                    loadFragment(savedInstanceState, HomeFragment(), R.string.title_home)
-                    fab.show()
+    private fun initNavigation() {
+        navigation.setOnNavigationItemSelectedListener(object : BottomNavigationView.OnNavigationItemSelectedListener {
+            override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+                when (p0.itemId) {
+                    navigation_home -> {
+                        loadFragment(home, R.string.title_home)
+                        fab.show()
+                        return true
+                    }
+                    navigation_explore -> {
+                        loadFragment(explore, R.string.title_explore)
+                        fab.hide()
+                        return true
+                    }
+                    navigation_message -> {
+                        loadFragment(message, R.string.title_message)
+                        fab.hide()
+                        return true
+                    }
+                    navigation_notification -> {
+                        loadFragment(notif, R.string.title_notification)
+                        fab.hide()
+                        return true
+                    }
+                    navigation_profile -> {
+                        loadFragment(profile, R.string.title_profile)
+                        fab.show()
+                        return true
+                    }
                 }
-                navigation_explore -> {
-                    loadFragment(savedInstanceState, ExploreFragment(), R.string.title_explore)
-                    fab.hide()
-                }
-                navigation_message -> {
-                    loadFragment(savedInstanceState, MessageFragment(), R.string.title_message)
-                    fab.hide()
-                }
-                navigation_notification -> {
-                    loadFragment(savedInstanceState, NotificationFragmentManager(), R.string.title_notification)
-                    fab.hide()
-                }
-                navigation_profile -> {
-                    loadFragment(savedInstanceState, ProfileFragment(), R.string.title_profile)
-                    fab.show()
-                }
-            }
-            true
-        }
-        navigation.selectedItemId = navigation_home
-    }
 
+                return false
+            }
+        })
+    }
 }
